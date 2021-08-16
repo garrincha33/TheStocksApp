1d41e97881d182ac074d36c4484b643fdbd7d533
Richard Price <mrspondooli@gmail.com>
2021-08-16 15:18:09 +0100

    inital commit project setup

---
+++ TheStocksApp/Controllers/WatchListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WatchListViewController.swift
+//  TheStocksApp
+//
+//  Created by Richard Price on 16/08/2021.
+//
+
+import UIKit
+
+class WatchListController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .purple
+        
+    }
+    
+}
---
+++ TheStocksApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheStocksApp
+//
+//  Created by Richard Price on 16/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       //step 2 test background
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
